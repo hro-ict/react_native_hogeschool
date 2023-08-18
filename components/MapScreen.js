786ddc0b535b9baa16ea461c851f15d5b49bfe4f
Yasar Unlu <57659697+hro-ict@users.noreply.github.com>
2023-08-18 19:39:57 +0200

    Update MapScreen.js

--- components/MapScreen.js
+++ components/MapScreen.js
@@ -1,66 +1,145 @@
-import * as React from 'react';
-import { Text, View, StyleSheet} from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
-import { MaterialCommunityIcons } from '@expo/vector-icons';
-import MapView from "react-native-maps";
-import Marker from "react-native-maps";
+import React from 'react';
+import { View } from 'react-native';
+import MapView, { Marker } from 'react-native-maps';
+import { Appbar  } from 'react-native-paper';
+// import * as Location from 'expo-location';
 
 
-export function MapScreen() {
-  const locationData = [
-    // {latitude: current_lat, longitude: current_long, desc: "current", color:"green"},
-    {latitude: 51.910387777297345, 
-      longitude: 4.4639805215644, 
-      desc: "Academieplein",
-      color: "blue",
-      adress: "G.J. de Jonghweg 4-6"
-    },
-      {latitude: 51.90668123869861, 
-      longitude: 4.459002341955153, 
-      desc:"Peter de Hoochweg",
-      color:"green",
-      adress: "Peter de Hoochweh 129"
-    },
-      {latitude: 51.91868541619013, 
-      longitude: 4.48439474997006, 
-      desc:"Wijnhaven 99",
-      color: "black",
-      adress: "Wijnhaven 99"
+const MapScreen = (props) => {
+  const data= {lat,long, desc, adress}= props.route.params
+  const [current_lat, set_current_lat]= React.useState(null)
+  const [current_long, set_current_long]= React.useState(null)
+  const [locations,setLocations]= React.useState("")
+
+
+  //get curren position
+React.useEffect(() => {
+    (async () => {
       
-    }
-  ];
-  return (
-    <MapView
-    zoomEnabled={true}
-    showsUserLocation={true}
-    style={styles.map}
-  initialRegion={{
-    latitude: 51.92482212888799,
-    longitude: 4.471815601820459,
-    latitudeDelta: 0.0922,
-    longitudeDelta: 0.0421,
-  }}
->
- <Marker 
-        //image= {require("./assets/banners/food-banner3.jpg")}
-   
-        title={"Title"}
-        description={"Adres"}
-        pinColor={"red"}
-         coordinate={{ 
-          latitude: locationData[0].latitude, longitude: locationData[0].longitude
-          }}/>
-</MapView>
-  );
-}
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        setErrorMsg('Permission to access location was denied');
+        return;
+      }
 
-const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-    },
-    map: {
-      width: '100%',
-      height: '80%',
-    },
-  });
+      let location = await Location.getCurrentPositionAsync({});
+      console.log(location.coords.latitude)
+    
+      set_current_lat(location.coords.latitude)
+      set_current_long(location.coords.longitude)
+    })();
+  }, []);
+
+const [theme, setTheme]= React.useState("")
+
+//get theme
+const get_theme = async () => {
+  try {
+    const value = await AsyncStorage.getItem('theme');
+    if (value !== null) {
+      // value previously stored
+      console.log(value)
+   
+      setTheme(value)
+    }
+  } catch (e) {
+    // error reading value
+  }
+};
+
+
+const getData_hotspots = async () => {
+
+  if (counter==0){
+      try {
+    const jsonValue = await AsyncStorage.getItem('hotspots');
+    console.log(jsonValue)
+    setLocations(JSON.parse(jsonValue))
+    set_counter(1)
+    return jsonValue != null ? JSON.parse(jsonValue) : null;
+  } catch (e) {
+    // error reading value
+  }
+  }
+  
+
+};
+
+//get hotspost from local database
+getData_hotspots()
+
+//get theme from local database
+get_theme()
+
+
+
+
+  return (
+    <View style={{ flex: 1,backgroundColor: theme === 'light' ? '#FFFFFF' : '#000000'}}>
+     <Appbar.Header >
+   
+      <Appbar.Content title="Hogeschool Rotterdam Locations"  />
+     
+    </Appbar.Header>
+      <MapView
+       showsUserLocation={true}
+        style={{ flex: 1}}
+        initialRegion={{
+          latitude: 51.92406928557815,
+          longitude: 4.492373052337382,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+      
+      >
+
+       <Marker
+           title={"Current position"}
+           pinColor={"green"}
+       
+         coordinate={{ 
+          latitude: current_lat, longitude: current_long
+          }}
+        />
+     
+           {
+  !lat ?
+  
+    locations.map((location, index) => (
+      
+       
+        <Marker 
+        //image= {require("./assets/banners/food-banner3.jpg")}
+    
+        title={location.desc}
+        description={location.adress}
+        pinColor={location.color}
+        key={index}
+          coordinate={{ 
+          latitude: location.latitude, longitude: location.longitude 
+          }}/>
+           
+         
+        ))
+        
+        :
+        <Marker
+           title={desc}
+        description={adress}
+         coordinate={{ 
+          latitude: lat, longitude: long
+          }}
+        />
+     
+        }
+
+        
+       
+      </MapView>
+
+
+    </View>
+  );
+};
+
+export default MapScreen;
